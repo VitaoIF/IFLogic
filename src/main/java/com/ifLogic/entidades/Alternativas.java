8ceb00879b22a836d29c67a6eaee9290a2975faf
VitaoIF <victormelo1212@gmail.com>
2026-02-24 03:47:15 -0300

    Feat: Adicao do Generated Value e relacionamento OneToMany

---
+++ src/main/java/com/ifLogic/entidades/Alternativas.java
@@ -0,0 +1,4 @@
+package com.ifLogic.entidades;
+
+public class Alternativas {
+}
--- src/main/java/com/ifLogic/entidades/Questoes.java
+++ src/main/java/com/ifLogic/entidades/Questoes.java
@@ -1,10 +1,12 @@
 package com.ifLogic.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_questoes")
@@ -13,9 +15,13 @@ import lombok.NoArgsConstructor;
 @Data
 public class Questoes {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String enunciado;
     private String explicacao;
     private String dificuldade;
 
+    @OneToMany(mappedBy = "questao")
+    private List<Alternativas> alternativa = new ArrayList<>();
 }

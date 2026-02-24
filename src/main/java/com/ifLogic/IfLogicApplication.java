0d8eb303287122f55b15950722cae22ad3d77909
VitaoIF <victormelo1212@gmail.com>
2026-02-24 02:46:24 -0300

    first commit

---
+++ src/main/java/com/ifLogic/IfLogicApplication.java
@@ -0,0 +1,13 @@
+package com.ifLogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfLogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifLogic/IfLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifLogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

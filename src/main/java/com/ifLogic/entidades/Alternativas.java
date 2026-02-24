3ec0ade7735258060293b19353cdf23808e8eebe
VitaoIF <victormelo1212@gmail.com>
2026-02-24 03:48:03 -0300

    Feat: Adicionando a entidiade alternativas e relacionamento many to one com questoes

--- src/main/java/com/ifLogic/entidades/Alternativas.java
+++ src/main/java/com/ifLogic/entidades/Alternativas.java
@@ -1,4 +1,24 @@
 package com.ifLogic.entidades;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_alternativas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Alternativas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String texto;
+    private boolean correta;
+
+    @ManyToOne
+    @JoinColumn(name = "questao_id")
+    private Questoes questao;
 }

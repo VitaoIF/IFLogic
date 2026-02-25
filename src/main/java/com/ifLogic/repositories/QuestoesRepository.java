78b196a25b61cbe2292f2d9c5c251d3cce0f0bcc
VitaoIF <victormelo1212@gmail.com>
2026-02-25 02:21:53 -0300

    Feat: adiciona repositorio para a entidade questoes

---
+++ src/main/java/com/ifLogic/repositories/QuestoesRepository.java
@@ -0,0 +1,7 @@
+package com.ifLogic.repositories;
+
+import com.ifLogic.entidades.Questoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestoesRepository extends JpaRepository<Questoes, Long> {
+}

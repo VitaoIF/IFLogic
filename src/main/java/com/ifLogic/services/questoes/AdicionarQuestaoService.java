919d7035debaa705e518ee79f391dd2c083d6dd4
VitaoIF <victormelo1212@gmail.com>
2026-02-25 03:46:40 -0300

    Feat: Adiciona o service para adicionar questoes

---
+++ src/main/java/com/ifLogic/services/questoes/AdicionarQuestaoService.java
@@ -0,0 +1,28 @@
+package com.ifLogic.services.questoes;
+
+import com.ifLogic.dto.QuestoesDTO;
+import com.ifLogic.entidades.Questoes;
+import com.ifLogic.repositories.QuestoesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AdicionarQuestaoService {
+
+    @Autowired
+    private QuestoesRepository repository;
+
+    @Transactional
+    public QuestoesDTO insert(QuestoesDTO dto){
+        Questoes entity = new Questoes();
+
+        entity.setEnunciado(dto.getEnunciado());
+        entity.setDificuldade(dto.getDificuldade());
+        entity.setId(dto.getId());
+
+        entity = repository.save(entity);
+        return new QuestoesDTO(entity);
+    }
+
+}

bdefb93f4bc8ea266f33326922a194f0ab7fd91c
VitaoIF <victormelo1212@gmail.com>
2026-02-25 03:31:18 -0300

    Feat: Adiciona controller para listar todas as questoes por uma URL especifica

--- src/main/java/com/ifLogic/controllers/questoes/ListarTodasQuestoes.java
+++ src/main/java/com/ifLogic/controllers/questoes/ListarTodasQuestoes.java
@@ -1,4 +1,23 @@
 package com.ifLogic.controllers.questoes;
 
+import com.ifLogic.dto.QuestoesDTO;
+import com.ifLogic.services.questoes.BuscarTodasQuestoesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/questoes")
 public class ListarTodasQuestoes {
+
+    @Autowired
+    private BuscarTodasQuestoesService service;
+
+    @GetMapping(value = "/todas")
+    public List<QuestoesDTO> findAll(){
+        return service.findAll();
+    }
 }

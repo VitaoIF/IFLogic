63cf1567d0ad8739dfd07134e4d153b037144919
VitaoIF <victormelo1212@gmail.com>
2026-02-25 03:30:08 -0300

    Feat: Adiciona service para paginar questoes

--- src/main/java/com/ifLogic/services/questoes/PaginarQuestoesService.java
+++ src/main/java/com/ifLogic/services/questoes/PaginarQuestoesService.java
@@ -1,4 +1,23 @@
 package com.ifLogic.services.questoes;
 
+import com.ifLogic.dto.QuestoesDTO;
+import com.ifLogic.entidades.Questoes;
+import com.ifLogic.repositories.QuestoesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class PaginarQuestoesService {
+
+    @Autowired
+    private QuestoesRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<QuestoesDTO> findAllPage(Pageable pageable){
+        Page<Questoes> resultado = repository.findAll(pageable);
+        return resultado.map(QuestoesDTO::new);
+    }
 }

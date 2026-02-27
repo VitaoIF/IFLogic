93b4a9f2b8e59667063d21ce9cd94b1d683ca5cc
VitaoIF <victormelo1212@gmail.com>
2026-02-27 01:31:33 -0300

    Feat: adiciona repositorios para alternativas

---
+++ src/main/java/com/ifLogic/repositories/AlternativasRepository.java
@@ -0,0 +1,7 @@
+package com.ifLogic.repositories;
+
+import com.ifLogic.entidades.Alternativas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlternativasRepository extends JpaRepository<Alternativas, Long> {
+}

b9a5d5e1e4dced48e4d66bb8d63ac52ab7285c18
VitaoIF <victormelo1212@gmail.com>
2026-02-27 01:30:48 -0300

    Feat: aidiciona usuarios repository

---
+++ src/main/java/com/ifLogic/repositories/UsuariosRepository.java
@@ -0,0 +1,9 @@
+package com.ifLogic.repositories;
+
+import com.ifLogic.entidades.Usuarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+}
